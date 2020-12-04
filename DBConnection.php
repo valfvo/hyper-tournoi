a3e9be56c30d5b7c09e791cc3238d59f7555d0ba
Paul Bunel <paul.bunel@etu.umontpellier.fr>
2020-12-04 17:56:16 +0000

    Création d'un fichier générique php se connectant à la base de données

---
+++ DBConnection.php
@@ -0,0 +1,15 @@
+<?php
+$dbname = "hypertournoi";
+$dsn = "pgsql:host=localhost;port=5432;dbname=$dbname;";
+$username = "postgres";
+$password = "hyper510";
+
+try {
+	$dbh = new PDO($dsn, $username, $password);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	// $dbh = null;
+	// echo "<p>Hyper Tournoi est bientôt disponible.</p>";
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+?>
